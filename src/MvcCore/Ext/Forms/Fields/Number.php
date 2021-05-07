be35d7817c4e484428c61e4aaf1e66aa6ca94b31
Tom Flídr <tomflidr@gmail.com>
2021-05-08 00:26:49 +0200

    Added field order property.

--- src/MvcCore/Ext/Forms/Fields/Number.php
+++ src/MvcCore/Ext/Forms/Fields/Number.php
@@ -44,7 +44,7 @@ implements	\MvcCore\Ext\Forms\Fields\IVisibleField,
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.1.4';
+	const VERSION = '5.1.5';
 
 	/**
 	 * Possible values: `number` and `range` in extended class.
@@ -80,6 +80,8 @@ implements	\MvcCore\Ext\Forms\Fields\IVisibleField,
 	 * Form field specific name, used to identify submitted value.
 	 * This value is required for all form fields.
 	 * @param  string                 $type 
+	 * Fixed field order number, null by default.
+	 * @param  int                    $fieldOrder
 	 * Form field type, used in `<input type="...">` attribute value.
 	 * Every typed field has it's own string value, but base field type 
 	 * `\MvcCore\Ext\Forms\Field` has `NULL`.
@@ -256,6 +258,7 @@ implements	\MvcCore\Ext\Forms\Fields\IVisibleField,
 
 		$name = NULL, 
 		$type = NULL, 
+		$fieldOrder = NULL,
 		$value = NULL, 
 		$title = NULL, 
 		$translate = NULL, 
